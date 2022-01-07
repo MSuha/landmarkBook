cbf3fae451ccaf77fe63608353fec7c739d56a06
MSuha <msuhademirel@gmail.com>
2022-01-07 23:38:43 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Muhammed Süha Demirel on 7.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
